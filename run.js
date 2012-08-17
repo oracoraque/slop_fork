ab3158ca5eaf389cfac7ff30e783b06025042e12
Slop <ond@seok.seok>
2012-08-17 15:40:38 -0700

    Autoload modules

--- run.js
+++ run.js
@@ -1,11 +1,23 @@
 
-var config = require('./config.json');
+var configPath = process.argv[2]
+? require('path').resolve(process.argv[2])
+: './config.json'
+
+var config = require(configPath);
 var Bot = require('./pr0kbot');
 
 config.autojoin = [];
-config.log = false
 
 var bot = new Bot(config);
+
+require('fs')
+.readdirSync('modules')
+.map(function(module) { return 'modules/'+module })
+.forEach(bot.use.bind(bot));
+
+bot.on('server notice', function(from, val) {
+    console.log('Server Notice', from, val)
+});
 
 bot.on('notice', function(from, val) {
     console.log('Notice', from, val)
@@ -15,5 +27,4 @@ bot.on('msg', function(from, val) {
     console.log('Msg', from, val);
 });
 
-//bot.use('modules/test');
-//bot.on('error', console.log)
+bot.on('error', console.log)
