951ea26b72cfcc5ad6e1c51eec4c17363a710e5d
Slop <ond@seok.seok>
2012-08-17 19:03:30 -0700

    NickServ identification

--- modules/auth.js
+++ modules/auth.js
@@ -2,15 +2,25 @@
  * This module is responsible
  * for catching nick-in-use 
  * conditions and randomizing
- * new nickname
+ * new nickname, and NickServ
+ * identification.
  */
 
 module.exports = function(bot) {
     var config = this.config;
     var nick = config.nick_name;
+
     bot.on('433', function(req) {
        var rand = Math.random().toString(36).substring(2, 6); 
        config.nick_name = [nick, rand].join('-');
        bot.auth();
+    });
+
+    bot.on('notice', function(req) {
+        if (req.from.nick === 'NickServ') {
+            if (req.val.startsWith('please choose a different nick')) {
+                bot.identify(config.password);
+            };
+        };
     });
 };
