3a21ac6e70b44d8bfb93504f146a66ccd3c9a708
Slop <ond@seok.seok>
2012-08-19 17:38:17 -0700

    Update run

--- run.js
+++ run.js
@@ -4,25 +4,65 @@
  */
 require('./strings');
 
+/**
+ * Load configuration
+ */
 var configPath = require('path')
 .resolve(process.argv[2] ? process.argv[2] : './config.json')
 
 var config = require(configPath);
-config.log = true;
 
+/**
+ * Set config programmatically
+ */
+
+//config.autojoin = ['#testx'];
+//config.nick_name = 'mynick';
+//config.user_name = 'myuser';
+//config.real_name = 'myname';
+//config.network = 'irc.butt.net';
+//config.master = 'Claude';
+
+/*8
+ * Initialize bot
+ */
 var Bot = require('./pr0kbot');
 var bot = new Bot(config);
 
-require('fs')
-.readdirSync('modules')
-.map(function(module) { return 'modules/'+module })
-.forEach(bot.use.bind(bot));
+/**
+ * Load modules
+ */
 
-bot.on('error', bot.log.bind(bot, 'error'));
+var fs = require('fs')
+var modules = 'modules/';
+var internal = modules+'internal/';
+var use = bot.use.bind(bot);
+var restrictJs = function(i) {
+    return /\.js$/.test(i);
+}
 
-bot.on('invite', function(req) {
-    console.log('Invite', req)
-    if (req.from.nick === 'Greeting') {
-        bot.join(req.val);
-    };
-});
+/**
+ * Load internal modules
+ */
+console.log('Loading internal modules...');
+
+fs.readdirSync(internal)
+.filter(restrictJs)
+.map(function(module) {return internal+module})
+.forEach(use)
+
+/**
+ * Load modules
+ */
+console.log('Loading modules...');
+
+fs.readdirSync(modules)
+.filter(restrictJs)
+.map(function(module) {return modules+module})
+.forEach(use)
+
+/**
+ * Finally, connect
+ *
+ */
+bot.connect();
