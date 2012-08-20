691d1384a3e9c19e04a3bbe7d236a9dd50fc5190
Slop <ond@seok.seok>
2012-08-19 21:38:39 -0700

    Master commands

--- modules/master.js
+++ modules/master.js
@@ -14,4 +14,38 @@ module.exports = function(bot) {
             bot.join(ev.val);
         };
     });
+
+    bot.on('.load', function(ev, res) {
+        if (!isMaster(ev.from.nick)) {
+            return;
+        };
+        var args = ev.cmd.argv;
+        if (args.length < 1) { return }
+        var name = args[0];
+
+        bot.load(name, function(err) {
+            if (err) {
+                res(err.toString());
+            }else {
+                res('Successfully loaded module "'+name+'"'); 
+            };
+        });
+    });
+
+    bot.on('.unload', function(ev, res) {
+        if (!isMaster(ev.from.nick)) {
+            return;
+        };
+        var args = ev.cmd.argv;
+        if (args.length < 1) { return }
+        var name = args[0];
+
+        bot.unload(name, function(err) {
+            if (err) {
+                res(err.toString());
+            }else {
+                res('Successfully unloaded module "'+name+'"'); 
+            };
+        });
+    });
 };
