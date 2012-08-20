387269900accd81e99668928575119256fb3eda0
Slop <ond@seok.seok>
2012-08-19 19:43:46 -0700

    Move utilities

---
+++ utils/codes.js
@@ -0,0 +1,36 @@
+/**
+ * Extend bot for response
+ * codes / types, and modes
+ */
+
+module.exports = function() {
+
+    this.CODES = {
+        '001':      'connect',
+        '433':      'nickinuse',
+        '353':      'names',
+        'NOTICE':   'notice',
+        'PRIVMSG':  'msg',
+        'INVITE':   'invite',
+        'MODE':     'mode',
+        'JOIN':     'join',
+        'PART':     'part',
+        'QUIT':     'quit'
+    };
+
+    this.MODES = {
+        '+v': 'voice',
+        '-v': 'devoice',
+        '+h': 'halfop',
+        '-h': 'dehalfop',
+        '+o': 'op',
+        '-o': 'deop',
+        '+b': 'ban',
+        '-b': 'unban',
+        '+m': 'mute',
+        '-m': 'unmute',
+        '+i': 'inviteonly',
+        '-i': 'deinviteonly'
+    };
+
+};
