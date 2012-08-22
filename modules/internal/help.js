83d9f7606844794fac8a2468f5571244f7fcdf71
Slop <ond@seok.seok>
2012-08-22 04:27:14 -0700

    Modules update

--- modules/internal/help.js
+++ modules/internal/help.js
@@ -17,24 +17,5 @@ module.exports = function(hook) {
         var help = this.getHelp(ev.cmd.argv[0]);
         res(help);
     });
-
-    var getCommands = function() {
-        var prefix = this.config.command_prefix;
-        var commands = [];
-        var modules = {};
-        var listeners = this.listeners;
-
-        listeners.forEach(function(i) {
-            var module = i.module;
-            if (modules[module]) {
-                return;
-            };
-            modules[module] = true;
-            commands.push(i.ev);
-        });
-
-        return commands.join(' ');
-    }.bind(this);
-
 };
 
